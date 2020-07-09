219efee918e4692bbd23f6f55ed4035229937ebc
icodeintl <katja@icodeit.nl>
2020-07-09 22:53:27 +0200

    home button commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'webapp';
+  showMenu = false;
+  darkModeActive = false;
+  constructor() {}
+
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
+  modeToggleSwitch(): void {
+    this.darkModeActive = !this.darkModeActive;
+  }
 }
+
+
